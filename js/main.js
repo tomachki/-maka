fea8ee5b5866b925fe1ab13c732c3adbdd0c43c8
tomachki <check.everything1@gmail.com>
2018-09-08 11:45:20 +0400

    not functional search bar

--- js/main.js
+++ js/main.js
@@ -2,13 +2,6 @@ function openMenu() {
     document.getElementById("side-menu").style.width = "300px";
 }
 
-//function dropdown() {
-//    document.getElementById("mob-dropdown").style.display = "block";
-//    document.getElementById("drop-1").style.backgroundColor = "#0E467D";
-//    document.getElementById("arrow").style.transform = "rotate(180deg)";
-//    document.getElementById("mini").id = "mini-1";
-//}
-//shrinkMenu
 function shrinkMenu(){
     document.getElementById("mob-dropdown").style.display = "none";
     document.getElementById("drop-1").style.backgroundColor = "inherit";
@@ -18,8 +11,23 @@ function shrinkMenu(){
 document.getElementById("mini").addEventListener("click", dropdown());
 document.getElementById("mini-1").addEventListener("click", shrinkMenu);
 
-
-
 function closeMenu() {
     document.getElementById("side-menu").style.width = "0";
 }
+
+//function search() {
+//    // div - iframe
+//    // get p: ps mivce klasi satauri. 
+//    var p = document.getElementsByTagName("p");
+//    var i;
+//    var input = document.getElementById("vidInput");
+//    var videos = document.getElementsByClassName("videos");
+//    
+//    for(i = 0; i < p.length; i++){
+//        if(p[i] == input.innerHTML){
+//            videos[i].style.display = "";
+//        } else{
+//            videos[i].style.display = "none";
+//        }
+//    }
+//}
